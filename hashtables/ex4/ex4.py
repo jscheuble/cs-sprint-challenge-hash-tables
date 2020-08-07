defda2845f52ab822ada1b323d1d6cacd05a201c
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-08-07 09:23:02 -0700

    exercise 4 stretch

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,23 @@
 def has_negatives(a):
     """
     YOUR CODE HERE
+
+    create a cache and empty list for result
+    loop over array
+        check for value in cache
+            if it's not there, add it
+            check if the negative value is in the cache
+                if true, append the absolute value to the result
     """
     # Your code here
+    cache = {}
+    result = []
+
+    for x in a:
+        if x not in cache:
+            cache[x] = x
+            if -x in cache:
+                result.append(abs(x))
 
     return result
 
