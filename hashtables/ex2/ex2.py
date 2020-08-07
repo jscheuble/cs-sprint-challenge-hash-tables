4dafd98a0534a5b1669ccfe41d5b0f821d2b6e3d
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-08-07 08:41:53 -0700

    exercise 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -8,7 +8,25 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
+
+    create a cache and a route list full of empty slots
+    iterate over list, cache all tickets. source as key, value as destination
+    flight with key of None will be our starting flight
+    loop over route list
+        set route at index i to the value stored in our current flight variable
+        set current to the cache value of current for our next destination
     """
     # Your code here
+    cache = {}
+    route = [None] * length
+
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+
+    current = cache['NONE']
+
+    for i in range(length):
+        route[i] = current
+        current = cache[current]
 
     return route
