eb9bf929308577e5a591c3211cf44795eaf18eae
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-08-07 08:25:04 -0700

    exercise 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,20 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
+
+    create a cache
+    loop through the array of weights
+        if the difference between limit and weight is in the cache already,
+            we have found our answer and we return (current index, cache[limit - weight] to target the other index)
+        cache[weight] = index
+    otherwise, return None because the pair we're looking for does not exist.
     """
     # Your code here
+    cache = {}
+
+    for i, weight in enumerate(weights):
+        if limit - weight in cache:
+            return (i, cache[limit - weight])
+        cache[weight] = i
 
     return None
